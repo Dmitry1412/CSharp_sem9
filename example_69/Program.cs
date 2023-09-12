5a019ea03b8c302c5222b458988c775d9d6e4fa4
Dmitry <sicily007@rambler.ru>
2023-09-12 20:48:42 +0300

    sem9

---
+++ example_69/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+System.Console.WriteLine(Pow(2, 2));
+
+int Pow(int num, int rank)
+{
+    if (rank == 0)
+    {
+        return 1;
+    }
+    if (rank == 1)
+    {
+        return num;
+    }
+    if (rank < 0)
+    {
+        return (1 / (num * Pow(num, rank + 1)));  
+    }
+    return (num * Pow(num, rank - 1));
+
+}
